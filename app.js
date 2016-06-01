9f1856359a12cca96968a1eeb5a2545cafe78265
lowitzki <lowitzki@gmail.com>
2016-06-01 18:40:06 +0800

    update develop

--- app.js
+++ app.js
@@ -10,14 +10,44 @@ var ACCESS_CONTROL_ALLOW_ORIGIN = false;
 
 app.use(express.static(__dirname + '/public'));
 
-app.post('/upload', multipart(), function (req, res) {
-
+app.post('/upload', multipart(), function(req, res) {
+    flow.post(req, function(status, filename, original_filename, identifier) {
+        console.log('POST', status, original_filename, identifier);
+        if (ACCESS_CONTROL_ALLOW_ORIGIN) {
+            res.header("Access-Control-Allow-Origin", "*");
+        }
+        res.status(status).send();
+    });
 });
 
-app.get('/upload', function (req, res) {
-
+app.options('/upload', function(req, res) {
+    console.log('OPTIONS');
+    if (ACCESS_CONTROL_ALLOW_ORIGIN) {
+        res.header("Access-Control-Allow-Origin", "*");
+    }
+    res.status(200).send();
 });
 
-app.listen(PORT, function () {
+app.get('/upload', function(req, res) {
+    flow.get(req, function(status, filename, original_filename, identifier) {
+        console.log('GET', status);
+        if (ACCESS_CONTROL_ALLOW_ORIGIN) {
+            res.header("Access-Control-Allow-Origin", "*");
+        }
+
+        if (status == 'found') {
+            status = 200;
+        } else {
+            status = 204;
+        }
+
+        res.status(status).send();
+    });
+});
+
+app.get('/download/:identifier&:fileName', function(req, res) {
+    flow.write(req.params.identifier,req.paramsfileName, res);
+});
+app.listen(PORT, function() {
     console.log(`Listen to http://localhost:${PORT}`);
 });
